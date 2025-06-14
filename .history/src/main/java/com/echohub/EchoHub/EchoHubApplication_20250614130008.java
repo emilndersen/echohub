859ae6fd2b44ec25d2f35a6ef5d2baed8fcd1785
Emil Nasinnikov <emilnsinnk@gmail.com>
2025-06-14 13:21:56 +0000

    Initial commit — Spring Boot blog base

---
+++ .history/src/main/java/com/echohub/EchoHub/EchoHubApplication_20250614130008.java
@@ -0,0 +1,13 @@
+package com.echohub.EchoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoHubApplication {
+
+	void main(String[] args) {
+		SpringApplication.run(EchoHubApplication.class, args);
+	}
+
+}
