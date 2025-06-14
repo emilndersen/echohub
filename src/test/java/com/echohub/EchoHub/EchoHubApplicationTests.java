0e67acf5483c41c72355ecfbcfa1fe5406c5eca4
Emil Nasinnikov <emilnsinnk@gmail.com>
2025-06-14 13:21:07 +0000

    Add proper .gitignore for Spring Boot Java project

---
+++ src/test/java/com/echohub/EchoHub/EchoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echohub.EchoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
